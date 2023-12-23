5c32926d05471460ef0b7e607dd792870cf05f93
Alex Rud <lexa.roach@gmail.com>
2023-12-23 21:30:19 +0300

    feat: добавить реализацию препроцессора

--- main.cpp
+++ main.cpp
@@ -14,14 +14,83 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+    const regex r_1(R"(\s*#\s*include\s*<([^>]*)>\s*)");
+    const regex r_2(R"(\s*#\s*include\s*\"([^"]*)\"\s*)");
+
+    ifstream fin(in_file);
+    if (!fin) { return false; }
+
+    ofstream fout(out_file, ios::app);
+    if (!fout) { return false; }
+
+    int line_count = 0;
+
+    while (fin) {
+        string str;
+        getline(fin, str);
+        ++line_count;
+
+        smatch m;
+        path matched_p;
+
+        if (regex_match(str, m, r_1)) {
+            bool is_exist = false;
+            matched_p = m[1].str();
+
+            for (const auto& dir : include_directories) {
+                path p = dir / matched_p;
+                if (filesystem::exists(p)) {
+                    is_exist = true;
+                    Preprocess(p, out_file, include_directories);
+                    break;
+                }
+            }
+
+            if (!is_exist) {
+                cout << "unknown include file "s << m[1] << " at file "s << in_file.string() << " at line "s << line_count << endl;
+                return false;
+            }
+        } else if (regex_match(str, m, r_2)) {
+            matched_p = m[1].str();
+            path p = in_file.parent_path() / matched_p.parent_path() / matched_p.filename();
+
+            if (filesystem::exists(p)) {
+                Preprocess(p, out_file, include_directories);
+            } else {
+                bool is_exist = false;
+
+                for (const auto& dir : include_directories) {
+                    path under_p = dir / matched_p.parent_path() / matched_p.filename();
+                    if (filesystem::exists(under_p)) {
+                        is_exist = true;
+                        Preprocess(under_p, out_file, include_directories);
+                        break;
+                    }
+                }
+
+                if (!is_exist) {
+                    cout << "unknown include file "s << m[1] << " at file "s << in_file.string() << " at line "s << line_count << endl;
+                    return false;
+                }
+            }
+        }
+
+        if (fin && !regex_match(str, m, r_1) && !regex_match(str, m, r_2)) {
+            fout << str << endl;
+        }
+    }
+
+    fin.close();
+    fout.close();
+
+    return true;
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
 
-    // конструируем string по двум итераторам
-    return {(istreambuf_iterator<char>(stream)), istreambuf_iterator<char>()};
+    return { (istreambuf_iterator<char>(stream)), istreambuf_iterator<char>() };
 }
 
 void Test() {
@@ -34,32 +103,32 @@ void Test() {
     {
         ofstream file("sources/a.cpp");
         file << "// this comment before include\n"
-                "#include \"dir1/b.h\"\n"
-                "// text between b.h and c.h\n"
-                "#include \"dir1/d.h\"\n"
-                "\n"
-                "int SayHello() {\n"
-                "    cout << \"hello, world!\" << endl;\n"
-                "#   include<dummy.txt>\n"
-                "}\n"s;
+            "#include \"dir1/b.h\"\n"
+            "// text between b.h and c.h\n"
+            "#include \"dir1/d.h\"\n"
+            "\n"
+            "int SayHello() {\n"
+            "    cout << \"hello, world!\" << endl;\n"
+            "#   include<dummy.txt>\n"
+            "}\n"s;
     }
     {
         ofstream file("sources/dir1/b.h");
         file << "// text from b.h before include\n"
-                "#include \"subdir/c.h\"\n"
-                "// text from b.h after include"s;
+            "#include \"subdir/c.h\"\n"
+            "// text from b.h after include"s;
     }
     {
         ofstream file("sources/dir1/subdir/c.h");
         file << "// text from c.h before include\n"
-                "#include <std1.h>\n"
-                "// text from c.h after include\n"s;
+            "#include <std1.h>\n"
+            "// text from c.h after include\n"s;
     }
     {
         ofstream file("sources/dir1/d.h");
         file << "// text from d.h before include\n"
-                "#include \"lib/std2.h\"\n"
-                "// text from d.h after include\n"s;
+            "#include \"lib/std2.h\"\n"
+            "// text from d.h after include\n"s;
     }
     {
         ofstream file("sources/include1/std1.h");
@@ -71,22 +140,22 @@ void Test() {
     }
 
     assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
-                                  {"sources"_p / "include1"_p,"sources"_p / "include2"_p})));
+        { "sources"_p / "include1"_p,"sources"_p / "include2"_p })));
 
     ostringstream test_out;
     test_out << "// this comment before include\n"
-                "// text from b.h before include\n"
-                "// text from c.h before include\n"
-                "// std1\n"
-                "// text from c.h after include\n"
-                "// text from b.h after include\n"
-                "// text between b.h and c.h\n"
-                "// text from d.h before include\n"
-                "// std2\n"
-                "// text from d.h after include\n"
-                "\n"
-                "int SayHello() {\n"
-                "    cout << \"hello, world!\" << endl;\n"s;
+        "// text from b.h before include\n"
+        "// text from c.h before include\n"
+        "// std1\n"
+        "// text from c.h after include\n"
+        "// text from b.h after include\n"
+        "// text between b.h and c.h\n"
+        "// text from d.h before include\n"
+        "// std2\n"
+        "// text from d.h after include\n"
+        "\n"
+        "int SayHello() {\n"
+        "    cout << \"hello, world!\" << endl;\n"s;
 
     assert(GetFileContents("sources/a.in"s) == test_out.str());
 }
